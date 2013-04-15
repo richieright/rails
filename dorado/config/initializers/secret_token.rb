c366e490c3afed697c8de4c0131bdff298cd46fe
Ming Ming <richieright@gmail.com>
2013-04-15 17:06:19 +0800

    first commit

---
+++ dorado/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dorado::Application.config.secret_token = '92991e3fb2723a88339970469e9f23aa58d26315ff2a6ca1b9953f1813ab3dc2cfcdad4bb04236602649532c961602254ff852083fcc903c338d01e6f58fc07c'
